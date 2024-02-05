ff4f71178e7345fee78c9cfcec134b9208a6f674
DiamondOreDev <harsh.diamondore@gmail.com>
2024-02-05 11:48:42 +0530

    Add Jobs file

--- Client/src/Pages/AdminPage/Addjobs.jsx
+++ Client/src/Pages/AdminPage/Addjobs.jsx
@@ -6,7 +6,7 @@ import { useJwt } from "react-jwt";
 import { useNavigate } from "react-router-dom";
 
 const AddJobs = () => {
-  const [sheet, setsheet] = useState(null);
+  const [sheet, setSheet] = useState(null);
   const [sheeturl, setsheeturl] = useState(null);
   const [upload, setupload] = useState(null);
 
@@ -97,8 +97,9 @@ const AddJobs = () => {
       <h1 className='text-3xl font-serif text-blue-950 text-center'>Upload youe Excel Sheet</h1>
       <div className='w-48 bg-blue-950 h-0.5 text-center mx-auto  my-3'></div>
 
+      <div>
       <div class="flex items-center justify-center w-full px-12 py-4">
-        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-blue-950 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <label htmlFor="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-blue-950 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
           <div class="flex flex-col items-center justify-center pt-5 pb-6">
             <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
@@ -106,10 +107,11 @@ const AddJobs = () => {
             <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
             <p class="text-xs text-gray-500 dark:text-gray-400">Only Excel sheets</p>
           </div>
-          <input id="dropzone-file" type="file" class="hidden" onChange={(e) => setsheet(e.target.value)} />
+          <input id="dropzone-file" type="file" class="hidden" name='sheet' onChange={(e) => setSheet(e.target.files[0])} />
         </label>
       </div>
       <button type='submit' className='bg-blue-950 text-white p-2 px-12 flex items-center justify-center mx-auto rounded-md' onClick={handleUploadsheet}>upload</button>
+      </div>
       {upload && (
         <div className="flex items-center justify-center bg-red-300 p-4 rounded-md">
           <p className="text-center text-sm text-red-500">{upload}</p>
