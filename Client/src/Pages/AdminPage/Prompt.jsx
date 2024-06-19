07475da88ad01e541cca63c8a85ecc428b7fbd2d
zoya774 <zoya.rasonline@gmail.com>
2024-06-19 15:35:31 +0530

    z

--- Client/src/Pages/AdminPage/Prompt.jsx
+++ Client/src/Pages/AdminPage/Prompt.jsx
@@ -9,6 +9,7 @@ const Prompt = () => {
     const [sheet, setSheet] = useState(null);
     const [sheeturl, setsheeturl] = useState(null);
     const [showLoader, setShowLoader] = useState(false);
+    const [showLoader2, setShowLoader2] = useState(false);
     const [phone, setPhone] = useState('');
     const [profile, setProfile] = useState(null);
     const [error, setError] = useState('');
@@ -76,7 +77,7 @@ const Prompt = () => {
 
     const handleSearch = async () => {
         try {
-            setShowLoader(true);
+            setShowLoader2(true);
             const response = await axios.get(
                 `https://api.diamondore.in/api/admin-confi/findJobs/${phone}`
             );
@@ -100,7 +101,7 @@ const Prompt = () => {
                 }
             }
         } finally {
-            setShowLoader(false);
+            setShowLoader2(false);
         }
     };
 
@@ -172,7 +173,7 @@ const Prompt = () => {
             </div>
 
             {error && <p className="text-red-500 text-center">{error}</p>}
-            {showLoader ? (
+            {showLoader2 ? (
                 <div className="flex justify-center items-center">
                     <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin  fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
