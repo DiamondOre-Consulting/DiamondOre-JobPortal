f15d4dcb0c919978e4ab79b8cebbd4124bcdf38f
YASHJADON5 <jadonyash2@gmail.com>
2024-11-14 15:35:52 +0530

    fix bugs

--- Client/src/Pages/AdminPage/Employeesignup.jsx
+++ Client/src/Pages/AdminPage/Employeesignup.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useJwt } from "react-jwt";
 import axios from "axios";
-import AccountHandling from "../../../../Server/Models/AccountHandling";
 
 const Employeesignup = () => {
     const navigate = useNavigate();
