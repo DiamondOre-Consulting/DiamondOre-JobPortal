eef8506b68e97a3d95a9f8e60b074f8eb01dfbc9
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-12-10 18:03:17 +0530

    Added delete button and functionality with server

--- Server/Controllers/admin.js
+++ Server/Controllers/admin.js
@@ -1967,7 +1967,7 @@ router.delete("/delete/employee/:id", AdminAuthenticateToken, async (req, res, n
     const { id } = req.params
     console.log(id)
 
-    const oneEmployee = await Employees.findById({ _id: id });
+    const oneEmployee = await Employees.findByIdAndDelete({ _id: id });
 
     console.log(oneEmployee)
 
