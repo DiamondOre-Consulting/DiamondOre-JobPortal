3e284dc13e3d06587176fd746a4574401f6742e6
Yash-Jadon <yashjadon@diamondore.in>
2025-03-28 18:16:03 +0530

    backend error fixed

--- Server/Controllers/jobs.js
+++ Server/Controllers/jobs.js
@@ -224,7 +224,7 @@ router.post("/upload-job-excel", AdminAuthenticateToken, excelUpload.single('myF
     catch(err){
       errorArray.push({ error: err.message });
       console.error("Database insertion error:", err.message);
-      return response = res.status(400).json({message : "Database insertion error"});
+      return res.status(400).json({message : err.message});
     }
 
     return res.status(200).json({
